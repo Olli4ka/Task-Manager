8bc6e2b47240fb10a0056626b447019f86f0efde
Olga Panayot <panayot.olga@gmail.com>
2026-05-14 16:12:32 +0300

    feat: add Task model and apply migrations

--- src/app_taskmanager/models.py
+++ src/app_taskmanager/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name="Task name"
+    )
+    description = models.TextField(
+        blank=True,
+        verbose_name="Task description"
+    )
+    is_completed = models.BooleanField(
+        default=False,
+        verbose_name="Completed?"
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Creation date"
+    )
+
+    def __str__(self):
+        return self.title
